05a79be9d43ef620d5682608f18a3c5e9d3c0768
Hema123kamadolli <hema.kamadolli@mphasis.com>
2023-11-09 15:20:40 +0530

    Fibbonacci numbers

---
+++ Fibbonacci numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibbonacci_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 3: Fibonacci sequence
+            Console.WriteLine("\n\nFibonacci sequence (first 10 numbers):");
+            int n = 10;
+            int first = 0, second = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{first} ");
+                int temp = first;
+                first = second;
+                second = temp + second;
+            }
+            Console.ReadKey();
+        }
+    }
+}
